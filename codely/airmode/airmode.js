a94282d0e09f7cc7c1f5034d77ee736774e92127
Chinlol <chin544554@gmail.com>
2024-07-26 12:04:58 +0800

    12

--- codely/airmode/airmode.js
+++ codely/airmode/airmode.js
@@ -1,15 +1,53 @@
-
 // init ball and set h1 & h2 as models (default class)
 
-
 // init h1 with airMode
-const h1 = new Cotton('#image1', {
-  airMode: true
-})
+const h1 = new Cotton("#image1", {
+  airMode: true,
+});
 
 // init h2 with airMode reverse
-const h2 = new Cotton('#image2', {
+const h2 = new Cotton("#image2", {
   airMode: {
-    reverse: true
-  }
-})
+    reverse: true,
+  },
+});
+const h3 = new Cotton("#image3", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h4 = new Cotton("#image4", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h5 = new Cotton("#image5", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h6 = new Cotton("#image6", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h7 = new Cotton("#image7", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h8 = new Cotton("#image8", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h9 = new Cotton("#image9", {
+  airMode: {
+    reverse: true,
+  },
+});
+const h10 = new Cotton("#image10", {
+  airMode: {
+    reverse: true,
+  },
+});
